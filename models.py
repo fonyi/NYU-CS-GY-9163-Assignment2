0dc2b1795535cace7ea19f9a5619fdda54703444
Shane Fonyi <shanef@nyu.edu>
2019-10-06 12:17:23 -0400

    Create Flask Login Templates and Initial Routing

--- models.py
+++ models.py
@@ -1,1 +1,8 @@
+from . import db
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+    name = db.Column(db.String(1000))
+    phone = db.Column(db.Unicode(20),nullable = True)
