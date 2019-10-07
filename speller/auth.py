a60938347ce4594bd080bb2ba6c97ab65af7d3e6
Shane Fonyi <shanef@nyu.edu>
2019-10-07 14:02:20 -0400

    Fixed login issues

--- speller/auth.py
+++ speller/auth.py
@@ -9,6 +9,10 @@ from . import db
 auth = Blueprint('auth', __name__)
 
 
+@auth.route('/login')
+def login():
+    return render_template('login.html')
+
 @auth.route('/login', methods=['POST'])
 def login_post():
     email = request.form.get('email')
@@ -21,13 +25,16 @@ def login_post():
     # take the user supplied password, hash it, and compare it to the hashed password in database
     if not user or not check_password_hash(user.password, password): 
         flash('Please check your login details and try again.')
-        return redirect(url_for('auth.login')) # if user doesn't exist or password is wrong, reload the page
+        return redirect(url_for('auth.login_post')) # if user doesn't exist or password is wrong, reload the page
 
     # if the above check passes, then we know the user has the right credentials
     login_user(user, remember=remember)
     return redirect(url_for('main.profile'))
 
-
+@auth.route('/signup')
+def signup():
+    return render_template('signup.html')
+    
 @auth.route('/signup', methods=['POST'])
 def signup_post():
 
@@ -40,7 +47,7 @@ def signup_post():
 
     if user: # if a user is found, we want to redirect back to signup page so user can try again  
         flash('Email address already exists')
-        return redirect(url_for('auth.signup'))
+        return redirect(url_for('auth.signup_post'))
 
     # create new user with the form data. Hash the password so plaintext version isn't saved.
     new_user = User(email=email, name=name, password=generate_password_hash(password, method='sha256'), phone=phone)
@@ -49,7 +56,7 @@ def signup_post():
     db.session.add(new_user)
     db.session.commit()
 
-    return redirect(url_for('auth.login'))
+    return redirect(url_for('auth.login_post'))
 
 @auth.route('/logout')
 @login_required
